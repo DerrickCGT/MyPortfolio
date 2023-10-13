46e1954577b42420c7b5994ea1846dde27b5c6c2
Derrick Choong <dchoong90@gmail.com>
2023-10-13 13:02:49 +0800

    Initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { Link } from "react-scroll"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
